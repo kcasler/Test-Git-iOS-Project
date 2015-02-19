a6556c869cfc8f113c39b73e46977362e79dbd09
Kerry Casler <Kerry@Kerrys-Mac-mini.local>
2015-02-19 11:36:52 -0500

    added test class

---
+++ GITTest/FGTest.h
@@ -0,0 +1,13 @@
+//
+//  FGTest.h
+//  GITTest
+//
+//  Created by Kerry Casler on 2/19/15.
+//  Copyright (c) 2015 Kerry Casler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FGTest : NSObject
+
+@end
---
+++ GITTest/FGTest.m
@@ -0,0 +1,13 @@
+//
+//  FGTest.m
+//  GITTest
+//
+//  Created by Kerry Casler on 2/19/15.
+//  Copyright (c) 2015 Kerry Casler. All rights reserved.
+//
+
+#import "FGTest.h"
+
+@implementation FGTest
+
+@end
