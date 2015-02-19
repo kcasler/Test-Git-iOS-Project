95ac3ba867d4ece164514acda5e2148739810877
Kerry Casler <Kerry@Kerrys-Mac-mini.local>
2015-02-19 11:19:30 -0500

    Initial Commit

---
+++ GITTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GITTest
+//
+//  Created by Kerry Casler on 2/19/15.
+//  Copyright (c) 2015 Kerry Casler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GITTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GITTest
+//
+//  Created by Kerry Casler on 2/19/15.
+//  Copyright (c) 2015 Kerry Casler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
